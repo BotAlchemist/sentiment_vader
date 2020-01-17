587db2577186edb14cd9eb457db25bd94919cc67
Sumit Srivastava <sumvast@gmail.com>
2020-01-17 18:09:12 +0530

    Add files via upload

---
+++ sentiment_app.py
@@ -0,0 +1,33 @@
+from flask import Flask, render_template, request, url_for, redirect
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
+
+try:
+    vader = SentimentIntensityAnalyzer()
+except:
+    import nltk
+    nltk.download('vader_lexicon')
+
+
+app= Flask(__name__)
+
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+
+@app.route('/result', methods=['POST'])
+def result():
+    raw_text= [str(x) for x in request.form.values()]
+    text= [raw_text]
+
+    sentiment_result= vader.polarity_scores(text[0][0])
+
+    return render_template('home.html', sentiment_text= text[0][0],
+                           sentiment_result= sentiment_result)
+
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
