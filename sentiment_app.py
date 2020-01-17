445b16a00039c04c1a3f8e1852c9a7b620aadc2b
Sumit Srivastava <sumvast@gmail.com>
2020-01-17 20:59:45 +0530

    Update sentiment_app.py

--- sentiment_app.py
+++ sentiment_app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request, url_for, redirect
 from nltk.sentiment.vader import SentimentIntensityAnalyzer
 
+
 vader = SentimentIntensityAnalyzer()
 
 
@@ -19,9 +20,25 @@ def result():
     text= [raw_text]
 
     sentiment_result= vader.polarity_scores(text[0][0])
+    negative_value = round(sentiment_result['neg']*100,2)
+    neutral_value = round(sentiment_result['neu']*100,2)
+    positive_value = round(sentiment_result['pos']*100,2)
+    compound_value = sentiment_result['compound']*100
+    if compound_value >= 0.05:
+        overall_value= "Positive"
+    elif compound_value <= -0.05:
+        overall_value= "Negative"
+    else:
+        overall_value= "Neutral"
 
     return render_template('home.html', sentiment_text= text[0][0],
-                           sentiment_result= sentiment_result)
+                           sentiment_result= sentiment_result,
+                           negative_value= negative_value,
+                           neutral_value= neutral_value,
+                           positive_value= positive_value,
+                           compound_value= abs(round(compound_value,2)),
+                           overall_value=overall_value
+                           )
 
 
 
